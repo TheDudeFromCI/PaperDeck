d11ccca6ac3e10141a493d806f394f20854cc4cc
TheDudeFromCI <thedudefromci@gmail.com>
2020-05-20 04:12:38 -0700

    Changed logging frameworks.

--- Server/Shell/Start.sh
+++ Server/Shell/Start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-mvn -f Server/pom.xml package dependency:copy-dependencies && java -cp \"Server/target/*:Server/target/dependency/*\" net.whg.paperdeck.Main
+set -e
+
+mvn -f Server/pom.xml clean package dependency:copy-dependencies
+java -cp "Server/target/*:Server/target/dependency/*" net.whg.paperdeck.Main
